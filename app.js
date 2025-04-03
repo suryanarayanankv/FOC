68d58645eb6f843c3059125a166d97d7b836891d
suryanarayanankv <githubsurya@gmail.com>
2025-04-03 22:01:30 +0530

    deleted unwated files

--- app.js
+++
@@ -1,9 +0,0 @@
-const wrapper = document.querySelector('.sliderwrapper');
-const menuitem = document.querySelectorAll('.menuitem');
-
-
-menuitem.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        wrapper.style.transform = `translateX(${-100 * index}vw)`;
-    });
-});
